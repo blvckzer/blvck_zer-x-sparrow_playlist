f950f63fc9045c86237c65615ec8e018bacede86
blvckzer <adhuzadhuz6@gmail.com>
2024-07-17 12:12:55 +0530

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -11,38 +11,38 @@
 
 let allMusic = [
   {
-    name: "Harley Bird - Home",
-    artist: "Jordan Schor",
+    name: "TEST NAME",
+    artist: "TEST NAME",
     img: "music-1",
     src: "music-1"
   },
   {
-    name: "Ikson Anywhere – Ikson",
-    artist: "Audio Library",
+    name: "TEST NAME",
+    artist: "TEST NAME ",
     img: "music-2",
     src: "music-2"
   },
   {
-    name: "Beauz & Jvna - Crazy",
-    artist: "Beauz & Jvna",
+    name: "TEST NAME ",
+    artist: "TEST NAME ",
     img: "music-3",
     src: "music-3"
   },
   {
-    name: "Hardwind - Want Me",
-    artist: "Mike Archangelo",
+    name: "TEST NAME ",
+    artist: "TEST NAME",
     img: "music-4",
     src: "music-4"
   },
   {
-    name: "Jim - Sun Goes Down",
-    artist: "Jim Yosef x Roy",
+    name: "TEST NAME",
+    artist: "TEST NAME",
     img: "music-5",
     src: "music-5"
   },
   {
-    name: "Lost Sky - Vision NCS",
-    artist: "NCS Release",
+    name: "TEST NAME",
+    artist: "TEST NAME",
     img: "music-6",
     src: "music-6"
   },
